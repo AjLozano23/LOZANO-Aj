f38be08c568300bcadb9da5e3ad86058f3fa5e75
AjLozano23 <Ajlozano446@gmail.com>
2024-09-23 10:06:03 +0800

    Add files via upload

---
+++ LA1_LOZANO_CS2C/activity2.js
@@ -0,0 +1,10 @@
+//Activity 2 Numbers Activity
+let firstFavNum = 23;
+let secondFavNum = 32;
+
+console.log(firstFavNum + secondFavNum); // addition
+console.log(firstFavNum - secondFavNum); // subtraction
+console.log(firstFavNum * secondFavNum); // multiplication
+console.log(firstFavNum / secondFavNum); // division
+console.log(firstFavNum % secondFavNum); // modulus
+console.log(firstFavNum ** secondFavNum); // Exponentation(Power)
