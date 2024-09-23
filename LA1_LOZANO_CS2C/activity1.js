2ec2d532d86437f9417927a58366ca055c68c59f
AjLozano23 <Ajlozano446@gmail.com>
2024-09-23 09:54:16 +0800

    Delete LA1_LOZANO_CS2C/activity1.js

--- LA1_LOZANO_CS2C/activity1.js
+++
@@ -1,9 +0,0 @@
-let name = "Aaron John Cortado Lozano";
-let whatDoYouWannaBecomeInYourLife = "programmer";
-let gender = "male";
-let fbHandle = "AjTwentyThree23";
-
-console.log(name);
-console.log(whatDoYouWannaBecomeInYourLife);
-console.log(gender);
-console.log(fbHandle);
