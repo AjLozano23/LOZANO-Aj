22bb85307116d6c76814086ee7e8b158f578833e
AjLozano23 <Ajlozano446@gmail.com>
2024-09-23 10:11:11 +0800

    Add files via upload

---
+++ LA1_LOZANO_CS2C/activity3.js
@@ -0,0 +1,8 @@
+//Activity 3 Boolean
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 23;
+
+console.log(isJsProgrammingLanguage); //true
+console.log(isJsHard); //false
+console.log(favNumb + undefined); //NaN
