a14de4e73b2afe738628d21166b2f74d7b9fe800
AjLozano23 <Ajlozano446@gmail.com>
2024-09-23 10:46:48 +0800

    Add files via upload

---
+++ LA1_LOZANO_CS2C/activity5.js
@@ -0,0 +1,12 @@
+// Activity 5 Strings
+let favActorFirstName = "Keanu "; // First name
+let favActorLastName = "Reeves"; // Last name
+let fullName = favActorFirstName + favActorLastName; // concatenate first name and last name
+let uppercase = fullName.toUpperCase(); // capitalized version favorite actor
+let message = "My favorite actor is " + uppercase + " his best show is Silicon Valley";
+
+console.log(favActorFirstName); //Keanu
+console.log(favActorLastName); //Reeves
+console.log(fullName); //Keanu Reeves
+console.log(uppercase); //KEANU REEVES
+console.log(message); //My favorite actor is KEANU REEVES his best show is Silicon Valley
