8fe9d6555f586c8d49868d89893af7ea565ed268
AjLozano23 <Ajlozano446@gmail.com>
2024-09-23 10:24:37 +0800

    Add files via upload

---
+++ LA1_LOZANO_CS2C/activity4.js
@@ -0,0 +1,12 @@
+// Activity 4 Comparison or Relational Operators
+let firstFavNumb = 23; //first favorite number
+let secondFavNumb = 32; //second favorite number
+
+console.log(firstFavNumb > secondFavNumb); //false
+console.log(firstFavNumb < secondFavNumb); //true
+console.log(firstFavNumb >= secondFavNumb); // false
+console.log(firstFavNumb <= secondFavNumb); // true
+console.log(firstFavNumb === secondFavNumb); //false
+console.log(firstFavNumb == secondFavNumb); //false
+console.log(firstFavNumb !== secondFavNumb); //true
+console.log(firstFavNumb != secondFavNumb); //true
