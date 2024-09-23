37681eba11c91aa9482f5ccdc44c1dcab9eb7027
AjLozano23 <Ajlozano446@gmail.com>
2024-09-23 09:56:07 +0800

    Add files via upload

---
+++ LA1_LOZANO_CS2C/activity1.js
@@ -0,0 +1,10 @@
+let name = "Aaron John Cortado Lozano";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "male";
+let fbHandle = "AjTwentyThree23";
+
+// print all the variables
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(fbHandle);
