0bc500f102b6403c83de38ccdd857126e7bcf4f9
AjLozano23 <Ajlozano446@gmail.com>
2024-09-24 09:46:42 +0800

    Add files via upload

---
+++ LA2_LASTNAME_CS2C/CS2C_ALozano_C1-A1.js
@@ -0,0 +1,21 @@
+var name = "Aaron John Cortado Lozano";
+var birthDate = "April 23, 2003";
+var birthPlace = "Calaoa-an, Candon City, Ilocos Sur, Philippines 2710";
+var address = "Calaoa-an, Candon City, Ilocos Sur, Philippines, 2710";
+var course = "Bachelor of Science in Computer Science";
+var year = "2";
+var dreamJob = "Game Developer";
+
+console.log(name.toUpperCase() + " was born " + birthDate + " at " + birthPlace.toUpperCase() + ", and currently living at " + address.toUpperCase()+".");
+console.log(name.toLowerCase() + " is taking up " + course.toLowerCase() + " and dreams to be " + dreamJob + " after graduation.");
+
+var nameCM = "Aureo Leaño Capellan";
+var birthDateCM = "April 11, 2005";
+var birthPlaceCM = "Tamurong primero, Candon City, Ilocos Sur, Philippines 2710";
+var addressCM = "Tamurong primero, Ilocos Sur, Philippines, 2710";
+var courseCM = "Bachelor of Science in Computer Science";
+var yearCM = "2";
+var dreamJobCM = "Software Engineer";
+
+console.log(nameCM.toUpperCase() + " was born " + birthDateCM + " at " + birthPlaceCM.toUpperCase() + ", and currently living at " + addressCM.toUpperCase()+".");
+console.log(nameCM.toLowerCase() + " is taking up " + courseCM.toLowerCase() + " and dreams to be " + dreamJobCM + " after graduation.");
