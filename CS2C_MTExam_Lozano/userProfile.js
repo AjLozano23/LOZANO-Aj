53f83496cac1f0a25b258ec31523cc0512ce6774
AjLozano23 <Ajlozano446@gmail.com>
2024-10-31 10:31:33 +0800

    Add files via upload

---
+++ CS2C_MTExam_Lozano/userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Aaron John Lozano";
+let age = 21;
+let favoriteNumber = 23;
+let favoriteColor = "Violet";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
